fe10d09b647e9346bef23a84fa47180bc9e15b56
zhhhwang@umich.edu <zhhhwang@umich.edu>
2018-09-09 16:16:34 -0400

    Update code

--- NYCtaxifee/Code.R
+++ NYCtaxifee/Code.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(xgboost)
 library(tidyr)
 library(dplyr)
 library(lubridate)
@@ -10,11 +11,53 @@ library(lubridate)
 # Comments on the other directories
 train <- fread("Google Drive/Kaggle_Data/NYCtaxifee/train_small.csv")
 
-
 ######################
 # Data Preprocessing #
 ######################
 
-train <- train %>% separate(pickup_datetime, into = c("date", "time", "zone"), sep = " ") %>% select(-key, -zone) %>%
-  filter(pickup_longitude!=0) %>% filter(passenger_count < 7) %>% 
-  mutate(weekday = weekdays(as.Date(date)))
+# Const
+earthR <- 3958.7631
+maxPassenger <- 6
+halfCircleDegree <- 180
+jfk_coor_lati <- (40.6459 * pi)/halfCircleDegree
+jfk_coor_long <- (-73.7860 * pi)/halfCircleDegree
+lga_coor_lati <- (40.7721 * pi)/halfCircleDegree
+lga_coor_long <- (-73.8686 * pi)/halfCircleDegree
+ewr_coor_lati <- (40.6917 * pi)/halfCircleDegree
+ewr_coor_long <- (-74.1807 * pi)/halfCircleDegree
+feature <- c("passenger_count", "weekday", "miles", "timeFrame") 
+label <- c("fare_amount")
+
+# Funciton in calculating the haversine distance between two gps coordinates
+GPSdistance <- function(coor1_longitude, coor1_latitude, coor2_longitude, coor2_latitude){
+  longD <- coor2_longitude - coor1_longitude
+  latiD <- coor2_latitude - coor1_latitude
+  
+  haverSine <- sin(latiD / 2) * sin(latiD / 2) + cos(coor1_latitude) * cos(coor2_latitude) * sin(longD / 2) * sin(longD / 2)
+  haverAngle <- asin(sqrt(haverSine))
+  distance <- 2 * earthR * haverAngle
+  return(distance)
+}
+
+train <- train %>% separate(pickup_datetime, into = c("date", "time", "zone"), sep = " ") %>% 
+  select(-key, -zone) %>%
+  filter(!(pickup_longitude == 0 |  pickup_latitude == 0 | dropoff_latitude == 0 | dropoff_longitude == 0)) %>% filter(passenger_count <= maxPassenger) %>% 
+  mutate(pickup_longitude = (pickup_longitude * pi) / halfCircleDegree, 
+         pickup_latitude = (pickup_latitude * pi) / halfCircleDegree,
+         dropoff_longitude = (dropoff_longitude * pi) / halfCircleDegree, 
+         dropoff_latitude = (dropoff_latitude * pi) / halfCircleDegree) %>%
+  mutate(weekday = weekdays(as.Date(date))) %>% 
+  mutate(miles = GPSdistance(pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude)) %>%
+  mutate(timeFrame = substr(time, 1, 2)) %>% mutate(timeFrame = as.numeric(timeFrame))# %>%
+  #mutate(JFK = )
+  
+########################
+# Training and Testing #
+########################
+
+# Setting up cross validation index
+xgbModel <- xgboost(data = model.matrix(~ . + 0, train[, feature]), label = as.matrix(train[, label]), max.depth = 6, nrounds = 250, nthread = 4)
+
+
+  
+
